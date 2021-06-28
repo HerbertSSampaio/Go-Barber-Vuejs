3f5ec0df97e80b2135322f9aded570d71bdfa5af
HerbertSousa <herberti1405@outlook.com>
2021-06-28 09:59:09 -0300

    add vue router

---
+++ src/views/Home.vue
@@ -0,0 +1,17 @@
+<template>
+	<div class="home">
+		<LogIn />
+	</div>
+</template>
+
+<script>
+// @ is an alias to /src
+	import LogIn from '@/components/LogIn.vue';
+
+	export default {
+		name: 'Home',
+		components: {
+			LogIn,
+		},
+	};
+</script>
